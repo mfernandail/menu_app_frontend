3f26f4a11dd789cf5f6edd33aa10fb73ac4537f4
mfernandail <maria.ibanez.leiva@gmail.com>
2025-07-24 21:41:08 -0400

    Add changes

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route
-          path="/agregar"
+          path="/platos"
           element={
             <PrivateRoute>
               <Lunch />
